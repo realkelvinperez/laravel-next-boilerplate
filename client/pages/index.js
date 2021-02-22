9ef248d83a038bcd61be9068963ad6a68341ec6f
realkelvinperez <realkelvinperez@gmail.com>
2021-02-21 21:39:32 -0500

    first init

---
+++ client/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Chain Breaker ⛓</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+         <h1>Chain Breaker ⛓</h1>
+      </main>
+
+    </div>
+  )
+}
